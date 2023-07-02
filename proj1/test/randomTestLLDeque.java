03a4a37950454cd6955c78b781b52f8d6548513c
violet <evelgaden@gmail.com>
2023-07-02 21:07:46 +0800

    add link list deque randomized test

--- proj1/test/randomTestLLDeque.java
+++ proj1/test/randomTestLLDeque.java
@@ -1,15 +1,16 @@
 package test;
 
-import deque.ArrayDeque;
+import deque.LinkedListDeque;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-public class randomTestADeque {
+import static org.junit.Assert.assertEquals;
+
+public class randomTestLLDeque {
     @Test
     /** Randomized call addFirst, addLast, test the size and isEmpty. */
     public void randomizedSizeEmptyTest() {
-        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
         int size = 0;
 
         int N = 1000000;
@@ -18,27 +19,27 @@ public class randomTestADeque {
             if (operationNumber == 0) {
                 /* addFirst */
                 int randValue = StdRandom.uniform(0, 100);
-                ad1.addFirst(randValue);
+                lld1.addFirst(randValue);
                 size += 1;
             } else if (operationNumber == 1) {
                 /* addLast */
                 int randValue = StdRandom.uniform(0, 100);
-                ad1.addLast(randValue);
+                lld1.addLast(randValue);
                 size += 1;
             } else if (operationNumber == 2) {
                 /* size */
-                assertEquals("Size should be " + size + ".", size, ad1.size());
+                assertEquals("Size should be " + size + ".", size, lld1.size());
             } else if (operationNumber == 3) {
                 /* isEmpty */
-                assertEquals("Deque should be empty.", size == 0, ad1.isEmpty());
+                assertEquals("Deque should be empty.", size == 0, lld1.isEmpty());
             } else if (operationNumber == 4) {
                 /* removeFirst */
-                ad1.removeFirst();
+                lld1.removeFirst();
                 size -= 1;
                 if (size < 0) { size = 0; }
             } else if (operationNumber == 5) {
                 /* removeLast */
-                ad1.removeLast();
+                lld1.removeLast();
                 size -= 1;
                 if (size < 0) { size = 0; }
             }
