ea4ecefc85da1f436b5feecf0401e3ddd1cb7883
violet <evelgaden@gmail.com>
2023-07-06 20:08:35 +0800

    finish autograder

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -7,30 +7,47 @@ import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
     @Test
-    public void randTest() {
+    public void randomizedTestRF() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> cad1 = new ArrayDequeSolution<>();
 
+        String failedMessage = new String("\n");
         for (int i = 0; i < 10000; i += 1) {
-            int numberOperations = StdRandom.uniform(0, 5);
+            int numberOperations = StdRandom.uniform(0, 2);
 
             if (numberOperations == 0) {
+                /* Call addFirst. */
                 int randValue = StdRandom.uniform(0, 100);
-                sad1.addFirst(randValue);
-                cad1.addFirst(randValue);
-            } else if (numberOperations == 1) {
-                int randValue = StdRandom.uniform(0, 100);
-                sad1.addLast(randValue);
-                cad1.addLast(randValue);
-            } else if (numberOperations == 2) {
-                /* size */
-                assertEquals("Size is not correct.", cad1.size(), sad1.size());
-            } else if (numberOperations == 3 && cad1.size() != 0) {
-                /* removeFirst */
-                assertEquals("RemoveFirst is not correct.", cad1.removeFirst(), sad1.removeFirst());
-            } else if (numberOperations == 4 && cad1.size() != 0) {
-                assertEquals("RemoveLast is not correct.", cad1.removeLast(), sad1.removeLast());
+                sad1.addFirst(randValue); cad1.addFirst(randValue);
+                failedMessage += "addFirst(" + randValue + ")\n";
+
+            } else if (numberOperations == 1 && cad1.size() != 0) {
+                /* Call removeFirst. */
+                failedMessage += "removeFist()\n";
+                assertEquals(failedMessage, cad1.removeFirst(), sad1.removeFirst());
             }
         }
     }
+
+    @Test
+    public void randomizedTestRL() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> cad1 = new ArrayDequeSolution<>();
+
+        String failedMessage = new String("\n");
+        for (int i = 0; i < 10000; i += 1) {
+            int numberOperations = StdRandom.uniform(0, 2);
+
+            if (numberOperations == 0) {
+                /* Call addLast. */
+                int randValue = StdRandom.uniform(0, 100);
+                sad1.addLast(randValue); cad1.addLast(randValue);
+                failedMessage += "addLast(" + randValue + ")\n";
+
+            } else if (numberOperations == 1 && cad1.size() != 0) {
+                /* Call removeLast. */
+                failedMessage += "removeLast()\n";
+                assertEquals(failedMessage, cad1.removeLast(), sad1.removeLast());
+            }
+        }    }
 }
